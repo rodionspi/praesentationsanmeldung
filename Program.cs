3b1d11d36cb550335e40b13a96c998195f54abbf
Rodion <rodionspiridonov14@gmail.com>
2026-09-26 14:44:28 +0200

    Started adding of a cshtml etc.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
